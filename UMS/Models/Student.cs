039cb2b3e4c2df6bbfe9ee1361aadda32b860a0c
Farhan <himu.uiu@gmail.com>
2015-10-05 04:33:13 +0600

    student class added

---
+++ UMS/Models/Student.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UMS.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        
+    }
+}
